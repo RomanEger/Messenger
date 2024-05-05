d6ceb841a8dc8f37d4e316586dc8e786a45b6fdd
RomanEger <egerromaan@gmail.com>
2024-05-05 10:44:30 +0600

    добавил логгер

--- Messenger/Messenger.Backend/Hubs/ChatHub.cs
+++ Messenger/Messenger.Backend/Hubs/ChatHub.cs
@@ -3,13 +3,19 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Messenger.Backend.Hubs;
 
-public class ChatHub(ILogger logger) : Hub
+public class ChatHub : Hub
 {
+    private readonly ILogger _logger;
+
+    public ChatHub(ILogger logger)
+    {
+        _logger = logger;
+    }
     
     [Authorize]
     public async Task Send(string message, string userName)
     {
         await Clients.All.SendAsync("Receive", message, userName);
-        logger.LogInformation($"{nameof(Send)} message\r\n {message}	{userName}");
+        _logger.LogInformation($"{nameof(Send)} message\r\n {message}	{userName}");
     }
 }
