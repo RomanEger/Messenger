dafb86acc1fcda42742ee4ba44287532e97cebd1
RomanEger <egerromaan@gmail.com>
2024-06-27 15:39:31 +0600

    update unit tests

--- Messenger/UnitTests/TestUserSetData.cs
+++ Messenger/UnitTests/TestUserSetData.cs
@@ -37,18 +37,78 @@ public class TestUserSetData
     }
 
     [Theory]
+    [InlineData("@rfdjfrA231")]
+    [InlineData("@uerse3423A")]
+    [InlineData("@Awfqcds23")]
+    [InlineData("@Nsawe324")]
+    [InlineData("@Ldser52")]
+    public void Test_ValidUserName(string userName)
+    {
+        var user = new User()
+        {
+            UserName = userName
+        };
+        Assert.NotEqual(user.UserName, userName);
+    }
+
+    [Theory]
+    [InlineData("asdfdsD@")]
+    [InlineData("@g")]
+    [InlineData("@roman4k/")]
+    [InlineData(null)]
+    [InlineData("12pl]-=`")]
+    [InlineData("@12")]
+    [InlineData("@dsffdg231423#&@*$&@")]
+    public void Test_InvalidUserName(string userName)
+    {
+        var user = new User()
+        {
+            UserName = userName
+        };
+        Assert.NotEqual(user.UserName, userName);
+    }
+    
+    [Theory]
+    [InlineData("rfdjfrA231")]
+    [InlineData("@uerse3423A")]
+    [InlineData("Awf0-=qcds23")]
+    [InlineData("@N_12/sawe324")]
+    [InlineData("@Ldse#ds`r52")]
+    public void Test_ValidNickName(string nickName)
+    {
+        var user = new User()
+        {
+            NickName = nickName
+        };
+        Assert.NotEqual(user.NickName, nickName);
+    }
+
+    [Theory]
+    [InlineData("@g")]
+    [InlineData(null)]
+    [InlineData("12")]
+    public void Test_InvalidNickName(string nickName)
+    {
+        var user = new User()
+        {
+            NickName = nickName
+        };
+        Assert.NotEqual(user.NickName, nickName);
+    }
+    
+    [Theory]
     [InlineData("+79513121243")]
     [InlineData("+79509858746")]
     [InlineData("+79335769267")]
     [InlineData("+71234569856")]
     [InlineData("+79567216784")]
-    public void Test_ValidPhoneNumber(string phonenumber)
+    public void Test_ValidPhoneNumber(string phoneNumber)
     {
         var user = new User()
         {
-            PhoneNumber = phonenumber
+            PhoneNumber = phoneNumber
         };
-        Assert.NotEqual(user.PhoneNumber, phonenumber);
+        Assert.NotEqual(user.PhoneNumber, phoneNumber);
     }
 
     [Theory]
@@ -57,13 +117,13 @@ public class TestUserSetData
     [InlineData("=75421672839")]
     [InlineData("+01111101018")]
     [InlineData("$951g7691023")]
-    public void Test_InvalidPhoneNumber(string phonenumber)
+    public void Test_InvalidPhoneNumber(string phoneNumber)
     {
         var user = new User()
         {
-            PhoneNumber = phonenumber
+            PhoneNumber = phoneNumber
         };
-        Assert.NotEqual(user.PhoneNumber, phonenumber);
+        Assert.NotEqual(user.PhoneNumber, phoneNumber);
     }
 
     [Theory]
