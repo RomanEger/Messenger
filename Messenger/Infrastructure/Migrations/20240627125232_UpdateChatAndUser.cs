12168d8211fcbc66aa6ea5cd93ecb2f333875d96
RomanEger <egerromaan@gmail.com>
2024-06-28 10:29:18 +0600

    add migrations

---
+++ Messenger/Infrastructure/Migrations/20240627125232_UpdateChatAndUser.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations.ApplicationDb
+{
+    /// <inheritdoc />
+    public partial class UpdateChatAndUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NickName",
+                table: "Users",
+                type: "character varying(120)",
+                maxLength: 120,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ChatName",
+                table: "Chats",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chats_ChatName",
+                table: "Chats",
+                column: "ChatName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Chats_ChatName",
+                table: "Chats");
+
+            migrationBuilder.DropColumn(
+                name: "NickName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ChatName",
+                table: "Chats");
+        }
+    }
+}
