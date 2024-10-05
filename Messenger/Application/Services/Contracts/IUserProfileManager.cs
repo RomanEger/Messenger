63e8b726b400554f6d23c461c778d79980e23152
RomanEger <egerromaan@gmail.com>
2024-10-05 10:07:08 +0600

    add possibility to change nickname

--- Messenger/Application/Services/Contracts/IUserProfileManager.cs
+++ Messenger/Application/Services/Contracts/IUserProfileManager.cs
@@ -3,4 +3,6 @@ namespace Application.Services.Contracts;
 public interface IUserProfileManager
 {
     Task<bool> ChangeUserName(string userName, string newUserName);
+
+    Task<bool> ChangeNickName(string userName, string newNickName);
 }
